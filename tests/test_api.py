7fbccf0c76338f6d4bd3aa4b3245e6187efc31b4
JoaoPauloZcyberZX <joaopaulosantos59@hotmail.com>
2025-09-03 00:36:45 -0300

    Create test_api.py

---
+++ tests/test_api.py
@@ -0,0 +1,22 @@
+import pytest
+from APIFIISPUCPR import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_fiis_api(client):
+    response = client.get('/api/fiis')
+    assert response.status_code == 200
+
+    data = response.get_json()
+    assert isinstance(data, list)
+
+    if data:
+        item = data[0]
+        assert "ticker" in item
+        assert "price" in item
+        assert "dividend_yield" in item
+        assert "last_dividend" in item
+        assert "last_dividend_date" in item
